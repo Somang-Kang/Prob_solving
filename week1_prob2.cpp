8c0680e2dfb7c75857e3d2a8fd84eb7bc2ef1eb8
somang <somang@nsl.inha.ac.kr>
2021-09-03 17:52:59 +0900

    first commit

--- week1_prob2.cpp
+++ week1_prob2.cpp
@@ -3,9 +3,47 @@
 //
 
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 int main(){
-    cout<<"hello";
+    int testcase;
+    cin>>testcase;
+    while(testcase--){
+        int qnum;
+        cin>>qnum;
+        vector<int> block_vec;
+        int min = 0;
+        int max = 0;
+
+        while(qnum--){
+            int number;
+            cin>>number;
+            if(number == 1){
+                int num;
+                cin>>num;
+                if(block_vec.size()==0){min = num;max = num;}
+                else{
+                    if(num>max) max = num;
+                    if(num<min) min = num;
+                }
+                block_vec.push_back(num);
+                cout<<min<<" "<<max<<"\n";
+            }
+            else if (number == 2){
+                if(block_vec.empty()) {}
+                else{
+                    block_vec.pop_back();
+                    min = 1000;max=0;
+                    for(int i = 0;i<block_vec.size();i++){
+                        if(block_vec[i]<min) min = block_vec[i];
+                        if(block_vec[i]>max) max = block_vec[i];
+                    }
+                }
+            }
+            else{
+
+            }
+        }
+    }
 }
